8e8d4a9d20928825611c5846dc1725098c9b78cd
ShivamSukhwal <shivam33@gmail.com>
2019-06-10 13:21:25 -0400

    Simple Javascript Game

---
+++ assets/javascript/app.js
@@ -0,0 +1,116 @@
+(function() {
+  function buildQuiz() {
+    // we'll need a place to store the HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // we'll want to store the list of answer choices
+      const answers = [];
+
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+        // if answer is wrong or blank
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
+  const myQuestions = [
+    {
+      question: "When Is Indian Independence Day?",
+      answers: {
+        a: "July 4th, 1776",
+        b: "August 15, 1947",
+        c: "May 5th, 1882",
+        d: "India was always Independent"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question:
+        "The City Montessori School is the worlds largest school in terms of studnets. How many students does it have?",
+      answers: {
+        a: "20,000",
+        b: "35,000",
+        c: "45,000",
+        d: "60,000"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "How many languages are recognized by the Indian Constitution?",
+      answers: {
+        a: "1",
+        b: "2",
+        c: "14",
+        d: "22"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "True or False? Chess was made in India.",
+      answers: {
+        a: "True",
+        b: "False"
+      },
+      correctAnswer: "a"
+    }
+  ];
+
+  // display quiz right away
+  buildQuiz();
+
+  // on submit, show results
+  submitButton.addEventListener("click", showResults);
+})();
